2422a00e0bec2f900239e7065062c3f95df130e8
xiaoeyu-961 <xien9782@gmail.com>
2026-09-22 14:34:20 +0800

    Fix historical index query range

--- scripts/fetch_cycle.py
+++ scripts/fetch_cycle.py
@@ -23,7 +23,7 @@ INDEXES = {
 }
 KLINE_URL = (
     "https://push2his.eastmoney.com/api/qt/stock/kline/get"
-    "?secid={secid}&klt=101&fqt=1&lmt=90"
+    "?secid={secid}&klt=101&fqt=1&lmt=90&beg=0&end=20500000"
     "&fields1=f1,f2,f3,f4,f5,f6"
     "&fields2=f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61"
 )
